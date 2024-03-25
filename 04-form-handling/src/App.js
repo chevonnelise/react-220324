6347e523de12cb54d68c037e039caf99d631c3ea
chevonnelise <elise.chevonne@gmail.com>
2024-03-25 04:43:22 +0000

    add radio buttons to 04-form-handling

---
+++ 04-form-handling/src/App.js
@@ -0,0 +1,12 @@
+import SurveyForm from "./SurveyForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <SurveyForm/>
+    </>
+  );
+}
+
+export default App;
